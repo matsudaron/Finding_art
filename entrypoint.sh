03bd4f9da8574c5f5cb6162294747732d1373d2d
坪松優李 <yuuritsubomatsu@tsubomatsuyuurinoMacBook-Air.local>
2024-06-28 21:14:14 +0900

    ‘’デプロイエラーの修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,10 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /app/tmp/pids/server.pid
-
-# Navigate to the app directory
-cd /app
+rm -f /Finding_art/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
+bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
